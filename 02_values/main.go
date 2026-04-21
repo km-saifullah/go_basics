11faa7568038e7a577c884896912a78482cbfb9b
km-saifullah <kmsaifullah16@gmail.com>
2026-04-21 20:06:47 +0600

    different types of values in go lang has added

---
+++ 02_values/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println(10)                // integer
+	fmt.Println(5.5)               // float
+	fmt.Println(true)              // boolean
+	fmt.Println("Hello Go")        // string
+	fmt.Println(time.Now().Date()) // date
+}
